804b0f6533c35abd6272a0abc807b4bf4bbda70c
bagasyuan10 <bagasyuan10@gmail.com>
2024-03-16 18:29:45 +0700

    update crud review

---
+++ app/Models/Review.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Review extends Model
+{
+    use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'reviews';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id',
+        'product_id',
+        'review_text',
+        'rating',
+    ];
+
+    /**
+     * Get the user that owns the review.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the product that the review belongs to.
+     */
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
